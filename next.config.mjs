aca1014d0d13606599819a96579e98de2b99c75a
PrivatePepega <privatepepega@yahoo.com>
2026-06-17 15:13:15 -0400

    removed out export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export', // For static site generation
     images: {
       unoptimized: true, // Required for GitHub Pages
     },
