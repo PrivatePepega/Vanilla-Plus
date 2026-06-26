108164a016bac4397254033174baa74539bfe5b0
PrivatePepega <privatepepega@yahoo.com>
2026-06-26 15:11:24 -0400

    updating test contracts

--- src/utils/contractAddressHardhat.js
+++ src/utils/contractAddressHardhat.js
@@ -1,14 +1,14 @@
-export const LocalBankSourceAddress = "0xd356697bD964f4071c5843e5E1E3CA4C7e2d69c3";
-export const LocalBankMoneyAddress = "0x93F06Ae2adae1081b6cd8E3F3A68EB6e1Ef32AFa";
-export const GuildBankPassportAddress = "0x9548C2C036058785f5849352a63D6415af66B43A"; // kept the latest GuildBank passport
-export const LocalBankTimeLockAddress = "0xF8728937B577aaE9A3f98F3074F1025faa2BAa8D";
-export const LocalBankGovernorAddress = "0x461D357CEF03477160580467F614479413481024";
-export const LocalBankGovernorElectionAddress = "0xc639C8DcfEf3e1e139f5b9636A95f8859682F480";
-export const LocalBankGovernorBallotAddress = "0xa34cA38C8FE31F5b4F76273AdD6d01046AC3210e";
-export const LocalBankBoDAddress = "0xa649e9bd40D47c4b75E0c8fA806520DF8287531b";
-export const LocalBankBoDTreasuryAddress = "0x8A34641bd37378B3659d88591Dc442ac755a3a18";
-export const LocalBankGovernorTreasuryAddress = "0x6c07c5a7dE57d1Cf01d60BF5Ca8A6474D8655AE0";
-export const LocalBankAccessManagerAddress = "0xf8f74A027fC2b4E48cb57926668780aC2e3fdF1b";
+export const LocalBankSourceAddress = "0x1d912898C5D0F018357dA48C65dF2C3c9E1172BB"; // LocalBankDAOcoin
+export const LocalBankMoneyAddress = "0x74DeBb1d6A257407de42a5C386AAaE410A8B20a0"; // LocalBankCredit
+export const GuildBankPassportAddress = "0xeE636C93c6adf9b9aa3ccE12c987Be8aFF376D39"; // kept the latest GuildBank passport
+export const LocalBankTimeLockAddress = "0xD872A5B28c07f248A6F4f1B599d1Ba1c646fa06a";
+export const LocalBankGovernorAddress = "0xDc3Cc94053EaCCEa5c8503ef6cC8757BbbC52766";
+export const LocalBankGovernorElectionAddress = "0xD0818e5cB0cc6EC5915371bE336F3c000c096a3c";
+export const LocalBankGovernorBallotAddress = "0x2f96FA3c5D87786e6E63F7F39A5048A57A634743";
+export const LocalBankBoDAddress = "0x0f9B91c77562bd7f72f433e837c5C96F87eb629b";
+export const LocalBankBoDTreasuryAddress = "0x6a12dcE2510A899eB006AEA7a947dDBE3F2342Be";
+export const LocalBankGovernorTreasuryAddress = "0x0502412e5D03D07F5EF894536B606174A73F056b";
+export const LocalBankAccessManagerAddress = "0x51ef574CE38979E3A6984bd2f1F864249DDDB662";
 
 export const contractAddresses = {
   Source: LocalBankSourceAddress,
