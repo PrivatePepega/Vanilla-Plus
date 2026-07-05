b03f133cafd0a481fbfc59301c78e9a3f8ca0708
privatepepega <privatepepega@yahoo.com>
2026-07-05 15:30:53 -0400

    adding security to our next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,20 @@ const nextConfig = {
     GB_SECRET_ACCESS_KEY: process.env.GB_SECRET_ACCESS_KEY,
     GB_SECRET_ID: process.env.GB_SECRET_ID,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
